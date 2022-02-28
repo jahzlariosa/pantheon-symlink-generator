19ddfaaebba36e83b5e9188bd905892548ba85b4
Joseph Lariosa <52392168+jahzlariosa@users.noreply.github.com>
2022-02-28 07:07:09 -0800

    Text Updates

--- index.php
+++ index.php
@@ -17,10 +17,10 @@
 
         <div class="alert alert-secondary">
           <h4>Important Note</h4>
-          Only use this on dev environment while on SFTP mode 
-          and don't forget to commit and deploy the changes
+          Only use this in dev environment while in SFTP mode and do not forget to commit and deploy the changes.
           <hr>
-          Note that this script only generate the symlinks and not the targets, Please do not forget to SFTP to the environment you are working on and make sure that the symlink target exists.
+          Please do not forget to SFTP to the environment you are working on and make sure that the symlink target exists.
+          If you need to know more about Pantheon Symlinks please see https://pantheon.io/docs/symlinks-assumed-write-access
           <hr>
           <h5>Extra Steps for Supported Plugins</h5>
 
@@ -34,7 +34,24 @@
             </h2>
             <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
               <div class="accordion-body">
-               Before generating WP Rocket Symlinks please edit <code>.gitignore</code> and comment out <code>wp-content/advanced-cache.php & wp-content/cache</code>
+              <ul>
+                <li>Before generating WP Rocket Symlinks please edit <code>.gitignore</code> and comment out <code>wp-content/advanced-cache.php &amp; wp-content/cache</code>
+</li>
+              </ul>
+              </div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-headingOne">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                WordFence
+              </button>
+            </h2>
+            <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+              <div class="accordion-body">
+              <ul>
+                <li>Before creating the WordFence Symlinks please deactivate the plugin first.</li>
+              </ul>
               </div>
             </div>
           </div>
